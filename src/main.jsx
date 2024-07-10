42ea49b5f30bb16dd0f8ac129600aa16882d1631
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-07-10 23:54:38 +0500

    7

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./components/App";
+import "./styles/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(<App />);
