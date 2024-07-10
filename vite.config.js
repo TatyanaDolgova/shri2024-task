bf0891d4ca9fc157c51ce028f6643f2563a864fc
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-07-10 21:17:12 +0500

    feat: очищен css

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { createHtmlPlugin } from 'vite-plugin-html'
+import { createHtmlPlugin } from 'vite-plugin-html';
+import purgeCss from 'vite-plugin-purgecss';
 
 export default defineConfig({
   base: './',
   plugins: [
     react(),
+    purgeCss(), ,
     createHtmlPlugin({
       minify: true
     })
