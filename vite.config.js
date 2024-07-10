517567efe97b5a79a923e66c85e61f58844c7491
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-07-10 14:42:44 +0500

    feat: минифицирован html

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true
+    })
+  ],
+});
