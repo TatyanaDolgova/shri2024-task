8887b3ac987d2c3ed6f88ab287562a63f03a8af6
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-07-11 15:50:10 +0500

    minimize css

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 
@@ -16,7 +17,8 @@ module.exports = {
   optimization: {
     minimize: true,
     minimizer: [
-      new TerserPlugin()
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
     ],
     splitChunks: {
       chunks: 'all',
