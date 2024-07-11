a1cb58dbd5df742ed8642aab4d1beb44def5fe02
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-07-11 11:08:49 +0500

    feat: attemt 1234567890

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./components/App.jsx";
+import "./styles/reset.css";
 import "./styles/styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
