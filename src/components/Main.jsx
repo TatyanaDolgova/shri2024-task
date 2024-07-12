fc5ec7034ad35ce8becb49aed9a84b8f4c34edb2
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-07-12 09:27:21 +0500

    укенгошлд

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -131,10 +131,6 @@ const TABS = {
   },
 };
 
-// for (let i = 0; i < 6; ++i) {
-//   TABS.all.items = [...TABS.all.items, ...TABS.all.items];
-// }
-
 const TABS_KEYS = Object.keys(TABS);
 
 const Main = () => {
